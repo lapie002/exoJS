1a6a74c03fc844220e99fee27b5b278b23374ae7
lapie002 <lapierre.bruno@gmail.com>
2017-03-09 13:52:20 +0100

    md

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 #Conditions :
 - Forkez ce repositorie et travaillez sur ce fork en pushant vos solutions directement dessus votre fork, ne clonez ou ne téléchargez pas car il ne sera pas possible de pushé directement sur le dépot de Simplon Narbonne, vous n'avez pas les droits, sur votre fork oui  
 
-Mettre vos solutions dans un fichier solutions.md à la racine de votre dépot  
+Mettre vos solutions dans un fichier solutions.md à la racine de votre dépot
+```javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+```  
 
 #REVISION JS:
 Reprenez le cours JS d'OpenClass et normalement le premier exo ne devrait pas trop vous poser de probleme :  
--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,9 @@ var members = ["Aida67", "lapie002", "anneserrano", "Jennysmille", "nunkabuk", "
 // SI la variable boucler vaut true ALORS
 function myFunction(){
           if(boucler)
-            // Bouclez sur le tableau que vous avez déclaré ci-dessus
+          // Bouclez sur le tableau que vous avez déclaré ci-dessus
           {
             for(var i=0;i<members.length;i++){
-                //members[i];
             // Mettre un switch pour qu'au moment où la boucle se trouve sur votre pseudo cela ajoute "Affiche " devant votre pseudo dans la console et sur l'écran et par defaut seulement le pseudo des autres
             switch(members[i]) {
               case "lapie002":
