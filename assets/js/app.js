437bb41d0defc45800b224f4c9e76c90241b51fa
lapie002 <lapierre.bruno@gmail.com>
2017-03-09 12:37:57 +0100

    exo js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,30 @@
 //REVISIONS
 // Declarez une variable nommée "boucler" contenant true
-
+var boucler = true;
 
 // Declarez un tableau members contenant Aida67, lapie002, anneserrano, Jennysmille, nunkabuk, RCosson, kaonb-ax, FerEmilie, crazychouwi, KiluaZoldyc, patatobeur, Sam11360, elo062, hermeline, Biciclet,
-
+var members = = ["Aida67", "lapie002", "anneserrano", "Jennysmille", "nunkabuk", "RCosson", "kaonb-ax", "FerEmilie", "crazychouwi", "KiluaZoldyc", "patatobeur", "Sam11360", "elo062", "hermeline", "Biciclet"];
 // SI la variable boucler vaut true ALORS
-
+if(boucler)
   // Bouclez sur le tableau que vous avez déclaré ci-dessus
-
+{
+  for(var i=0;i<members.length;i++){
+      members[i];
   // Mettre un switch pour qu'au moment où la boucle se trouve sur votre pseudo cela ajoute "Affiche " devant votre pseudo dans la console et sur l'écran et par defaut seulement le pseudo des autres
+  switch(members[i]) {
+    case "lapie002":
+        // sur ecran
+        
+        console.log("Affiche "+"lapie002");
 
+        break;
+    default:
+        console.log(members[i]);
+}
+
+
+  }
+}
 // FIN REVISIONS
 
 // COURS AJAX
